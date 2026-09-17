b8377e86aee2e1713e0004160be8c10581b0b92f
mustafa amanullah <mustafa357yt@gmail.com>
2026-09-17 09:20:24 +0500

    updated the model due to outdated model use

--- app.py
+++ app.py
@@ -137,7 +137,7 @@ def respond(
     prompt = f"You are a question-answering chatbot for Aether Corporations. Answer the questionas much as you can extract from the given context. Do not hint out that you are using context by saying 'Based on the context' or 'From the information provided' or related phrases, just answer the question. If the context is irrelevant, say that you cannot help with that query.\n\nContext:\n{context}\n\nQuestion: {message}"
 
     token = HF_TOKEN
-    client = InferenceClient(token=token, model="meta-llama/Llama-3.2-1B-Instruct")
+    client = InferenceClient(model="meta-llama/Meta-Llama-3-8B-Instruct")
 
     messages = [{"role": "system", "content": system_message}]
     for msg in history:
